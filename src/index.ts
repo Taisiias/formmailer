9f5f1e2cf4fe06e4d6c8c38b99ee41dc7f660161
Taisiia Sobchenko <taisiia.sobchenko@overvis.com>
2017-04-18 17:17:12 +0300

    Sending email with nodemailer.

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,11 @@
-// import * as http from "http";
-
-// import * as fs from "fs";
-
-// import * as url from "url";
-
-// import * as qs from "querystring";
-
-// import * as emailjs from "emailjs";
-
-// const server = http.createServer((req, resp) => {
-
-// });
-
 // import * as commandpost from "commandpost";
+
 import * as emailvalidator from "email-validator";
 import * as fs from "fs";
+import * as http from "http";
+import * as nodemailer from "nodemailer";
+import * as qs from "querystring";
+import * as url from "url";
 import * as winston from "winston";
 
 interface Config {
@@ -23,6 +14,8 @@ interface Config {
     recipientEmails: string | string[];
     fromEmail: string;
 }
+
+let config: Config;
 
 function validateFromEmail(fromEmail?: string): string {
     // Validating fromEmail
@@ -76,11 +69,11 @@ function ParseConfig(cf: Partial<Config>): Config {
         fromEmail: validateFromEmail(cf.fromEmail),
         recipientEmails: validateEmailRecipients(cf.recipientEmails as string),
         smtpHost: validateSmtpHost(cf.smtpHost),
-        smtpPort: validateSmtpPort(cf.smtpPort)};
+        smtpPort: validateSmtpPort(cf.smtpPort),
+    };
 }
 
-function run(): void {
-    let config: Config;
+function ReadConfig(): void {
     if (!fs.existsSync("./config.json")) {
         winston.error("Config file was not found.");
         return;
@@ -110,16 +103,67 @@ function run(): void {
         return;
     }
 
-    winston.log("info", "SMTP host is " + config.smtpHost);
-    winston.log("info", "SMTP port is " + config.smtpPort);
-    winston.log("info", "number of recipientEmails is " +
-        config.recipientEmails.length);
+    winston.info("SMTP host is " + config.smtpHost);
+    winston.info("SMTP port is " + config.smtpPort);
 }
 
-run();
+const server = http.createServer((req, res) => {
+    if (req.method !== "POST") { return; }
 
+    const urlPathName = url.parse(req.url as string, true);
+    res.writeHead(200, { "Content-Type": "application/json" });
 
+    if (urlPathName.pathname === "/receive") {
+        let bodyStr = "";
+        req.on("data", (chunk) => {
+            bodyStr += chunk.toString();
+            if (bodyStr.length > 1e6) {
+                // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+                req.connection.destroy();
+            }
+        });
 
+        req.on("end", () => {
+            const post = qs.parse(bodyStr);
+            sendEmail(post.user_name,
+                      post.user_mail,
+                      post.user_message,
+                      () => {
+                            winston.info("Redirecting to " + post._redirect);
+                            res.writeHead(301, { Location: post._redirect });
+                            res.end();
+                        });
+        });
+    } else {
+        res.end("asdf");
+    }
+});
+
+function sendEmail(userName: string,
+                   userMail: string,
+                   emailText: string,
+                   callbackFromSendMail: () => void): void {
+    ReadConfig();
+    const transporter = nodemailer.createTransport({
+        host: config.smtpHost,
+        port: config.smtpPort,
+    });
+
+    const emailMessage = {
+        from: userName + " " + userMail,
+        subject: "Test formmailer",
+        text: emailText,
+        to: config.recipientEmails,
+    };
+    transporter.sendMail(emailMessage,
+                         (err) => {
+            if (err) { winston.error(err.message); }
+            callbackFromSendMail();
+            winston.info("Message sent.");
+        });
+}
+
+server.listen(3000);
 
 // const root = commandpost
 //     .create<{ spice: string[]; cheese: string[]; config: string[]; }, { food: string[]; }>("dinner <food...>")
@@ -132,7 +176,6 @@ run();
 //             `Your dinner is ${args.food[0]} with ${opts.config[0] || "pepper"} and ${opts.cheese[0] || "mozarella"}!
 //             Lunch is ${args.food[1]}.`);
 //     });
-
 // commandpost
 //     .exec(root, process.argv)
 //     .catch((err) => {
