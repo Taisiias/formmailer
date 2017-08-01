26b62372b4cd17bc602db3ca90bfe75ab6f2b01b
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2017-08-01 19:38:23 +0300

    Merging default config object and json from file. Code clean up.

--- src/config.ts
+++ src/config.ts
@@ -1,42 +1,39 @@
-import * as emailvalidator from "email-validator";
 import * as fs from "fs";
-import * as winston from "winston";
 
 export interface Config {
-    smtpPort: number;
-    smtpHost: string;
-    recipientEmails: string | string[];
-    redirectFieldName: string;
     fromEmail: string;
-    logLevel: string;
-    subject: string;
     httpListenIP: string;
     httpListenPort: number;
     httpServerPath: string;
+    logLevel: string;
     maxHttpRequestSize: number;
+    recipientEmails: string | string[];
+    redirectFieldName: string;
+    smtpHost: string;
+    smtpPort: number;
+    subject: string;
 }
 
+const DefaultConfigObject: Object = {
+    httpListenIP: "0.0.0.0",
+    httpListenPort: 8080,
+    httpServerPath: "/",
+    logLevel: "debug",
+    maxHttpRequestSize: 1e6,
+    redirectFieldName: "_defaultRedirect",
+    smtpHost: "localhost",
+    smtpPort: 25,
+    subject: "Message from Formmailer",
+};
+
 const defaultConfigPath = "./config.json";
-const defaultHttpListenIp = "0.0.0.0";
-const defaultHttpListenPort = 8080;
-const defaulthttpServerPath = "/";
-const defaultMaxHttpRequestSize = 1e6;
-const defaultRedirectFieldName = "_defaultRedirect";
-const defaultSmtpHost = "localhost";
-const defaultSmtpPort = 25;
-const defaultSubject = "Message from Formmailer";
-const defaultLogLevel = "debug";
 
-// -- TODO: все функции называем в camelCase: readConfig()
-// -- TODO: эта функция должна возвращать Config (или кидать ошибки)
-export function readConfig(cfPath: string): Config {
+export function readConfig(cfPath?: string): Config {
 
-    // -- TODO: убираем лишний вывод.
     let cf: Config;
     const configFilePath = cfPath ? cfPath : defaultConfigPath;
 
     if (!fs.existsSync(configFilePath)) {
-        winston.warn(`Config file was not found.`);
         throw new Error(`Config file was not found.`);
     }
 
@@ -44,77 +41,23 @@ export function readConfig(cfPath: string): Config {
     try {
         fileContent = fs.readFileSync(configFilePath).toString();
     } catch (e) {
-        winston.warn(`Config file cannot be read. ${e.Message}`);
         throw new Error(`Config file cannot be read. ${e.Message}`);
     }
 
     let json: Object;
     try {
         json = JSON.parse(fileContent);
+        /* tslint:disable:no-any */
+        const mergedObject: {[k: string]: any} = Object.assign(DefaultConfigObject, json);
+        if (mergedObject.hasOwnProperty("smtpPort") && !mergedObject.fromEmail) {
+            throw new Error(`Property fromEmail is missing.`);
+        }
+        if (mergedObject.hasOwnProperty("recipientEmails") && !mergedObject.recipientEmails) {
+            throw new Error(`Property recipientEmails is missing.`);
+        }
+        cf = mergedObject as Config;
     } catch (e) {
-        winston.warn(`Config file cannot be parsed. ${e.Message}`);
         throw new Error(`Config file cannot be parsed. ${e.Message}`);
     }
-
-    try {
-        cf = parseConfig(json);
-    } catch (e) {
-        winston.error(`Error while parsing config file ${e.Message}`);
-        throw e;
-    }
     return cf;
-}
-
-function validateFromEmail(fromEmail?: string): string {
-    if (!fromEmail) {
-        throw new Error("Please add sender's email");
-    }
-    return fromEmail;
-}
-
-function validateEmailRecipients(recipientEmailsStr?: string | string[]): string[] {
-    // -- TODO: принимаем в конфиге string|string[] с емейлами. убираем ручную разбивку строки.
-    if (!recipientEmailsStr) {
-        throw new Error("Undefined recipient emails.");
-    }
-    let recipientEmailsArray: string[] = [];
-    if (typeof recipientEmailsStr === "string") {
-        recipientEmailsArray[0] = recipientEmailsStr;
-    } else {
-        recipientEmailsArray = recipientEmailsStr;
-    }
-    // -- TODO: для итерирования используем for of
-    for (const item of recipientEmailsArray) {
-        if (item === "") {
-            recipientEmailsArray.splice((recipientEmailsArray.indexOf(item)), 1);
-            continue;
-        }
-        if (!emailvalidator.validate(item.trim())) {
-            // -- TODO: используем динамические строки вместо ручной сборки строк.
-            throw new Error(`Incorrect email format: ${item}`);
-        }
-        recipientEmailsArray[recipientEmailsArray.indexOf(item)] = item.trim();
-    }
-    if (recipientEmailsArray.length === 0) {
-        throw new Error("Recipient emails list is empty.");
-    }
-    return recipientEmailsArray;
-}
-
-function parseConfig(cf: Partial<Config>): Config {
-
-    return {
-        fromEmail: validateFromEmail(cf.fromEmail),
-        httpListenIP: cf.httpListenIP ? cf.httpListenIP : defaultHttpListenIp,
-        httpListenPort: cf.httpListenPort ? cf.httpListenPort : defaultHttpListenPort,
-        httpServerPath: cf.httpServerPath ? cf.httpServerPath : defaulthttpServerPath,
-        logLevel: cf.logLevel ? cf.logLevel : defaultLogLevel,
-        maxHttpRequestSize:
-        cf.maxHttpRequestSize ? cf.maxHttpRequestSize : defaultMaxHttpRequestSize,
-        recipientEmails: validateEmailRecipients(cf.recipientEmails),
-        redirectFieldName: cf.redirectFieldName ? cf.redirectFieldName : defaultRedirectFieldName,
-        smtpHost: cf.smtpHost ? cf.smtpHost : defaultSmtpHost,
-        smtpPort: cf.smtpPort ? cf.smtpPort : defaultSmtpPort,
-        subject: cf.subject ? cf.subject : defaultSubject,
-    };
 }
