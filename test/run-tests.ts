88e2611e122a21c31775bb7eb943c983af5f5311
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2017-12-21 23:57:41 +0200

    Parsing test file.

--- test/run-tests.ts
+++ test/run-tests.ts
@@ -1,4 +1,19 @@
-import * as run from "../src/run";
+// import * as run from "../src/run";
+import * as fs from "fs";
 
 // tslint:disable-next-line:no-console
-console.log(run);
+// console.log(run);
+
+function parseTestFile(filePath: string): string {
+    const fileContent = fs.readFileSync(filePath).toString();
+    const fileParts = fileContent.split("---");
+    return fileParts[0].trim();
+}
+
+function runTests(): void {
+    const firstElement = parseTestFile("./test/test-cases/test-form.txt");
+    // tslint:disable-next-line:no-console
+    console.log(firstElement);
+}
+
+runTests();
