8abf76b60598acaad4a7d5bb043681e8fcbe97ab
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2017-12-27 23:27:44 +0200

    Preparing tests.

--- test/run-tests.ts
+++ test/run-tests.ts
@@ -1,5 +1,7 @@
 import * as fs from "fs";
+import winston = require("winston");
 import { createConfigObject } from "../src/config";
+
 // import { runHttpServers } from "../src/run";
 
 const TESTS_FOLDER_PATH = "./test/test-cases";
@@ -11,30 +13,44 @@ function parseTestFile(filePath: string): string[] {
 }
 
 function runTests(): void {
-    fs.readdirSync(TESTS_FOLDER_PATH).forEach((file) =>
-        runTest(TESTS_FOLDER_PATH + "/" + file));
+    let testPassed: boolean | Error;
+    let isError = false;
+    fs.readdirSync(TESTS_FOLDER_PATH).forEach((file) => {
+        winston.info(`Starting test: ${file}`);
+        testPassed = runTest(TESTS_FOLDER_PATH + "/" + file);
+        if (testPassed) {
+            winston.info(`Test result: OK`);
+        } else {
+            isError = true;
+            // const errorMessage = testPassed as Error;
+            winston.error(`An error occurred: ${(testPassed as Error).message}
+            StackTrace: ${(testPassed as Error).stack}`);
+        }
+    });
+    if (isError) {
+        winston.error(`One or more tests didn't pass.`);
+    } else {
+        winston.info(`All tests passed.`);
+    }
 }
 
 function runTest(fileName: string): true | Error {
-    // tslint:disable:no-console
-    console.log(`Starting test: ${fileName}`);
 
-    if (fileName === "./test/test-cases/test-form-2.txt") {
-        console.log(`Incorrect Filename error: ${fileName}`);
-        throw new Error("Incorrect Filename");
-    }
+    // if (fileName === "./test/test-cases/test-form-2.txt") {
+    //     winston.error(`Incorrect Filename error: ${fileName}`);
+    //     return new Error("Incorrect Filename");
+    // }
 
     const [configString, curl, response, emailSent] =
         parseTestFile(fileName);
 
     const cf = createConfigObject(configString);
 
-    console.log(`From Email: ${cf.fromEmail}`);
-    console.log(`Curl: ${curl}`);
-    console.log(`Response ${response}`);
-    console.log(`Email Sent ${emailSent}`);
+    winston.info(`From Email: ${cf.fromEmail}`);
+    winston.info(`Curl: ${curl}`);
+    winston.info(`Response ${response}`);
+    winston.info(`Email Sent ${emailSent}`);
 
-    console.log(`Test result: OK`);
     return true;
 }
 
