6596aa50e07770c7c68a1f95b8c3046ee0bb171b
Taisiia Sobchenko <taisiia.sobchenko@gmail.com>
2018-08-05 22:02:33 +0300

    Circle CI fix.

--- src/stream.ts
+++ src/stream.ts
@@ -3,7 +3,7 @@ import * as stream from "stream";
 export async function readReadable(s: stream.Readable, maxRequestSize: number): Promise<string> {
     return new Promise<string>((resolve, reject) => {
         let bodyStr = "";
-        s.on("data", (chunk) => {
+        s.on("data", (chunk: string) => {
             bodyStr += chunk.toString();
             if (bodyStr.length > maxRequestSize) {
                 // FLOOD ATTACK OR FAULTY CLIENT
